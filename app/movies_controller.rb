92177f9dca3093d2e3410fa0c0eb7c0fc9333acb
Omar Nava <omarnavaalvarez@gmail.com>
2018-04-02 15:36:33 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -41,14 +41,15 @@ def can_get_the_last_item_in_the_database
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.size
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.find(1).title
 end
 
 def can_find_by_multiple_attributes
+  Movie.find_by(tile: "Title", release_date: 2000, director: "Me")
   # title == "Title"
   # release_date == 2000
   # director == "Me"
